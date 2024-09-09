256260e70e8661d83a10212eec5fc0a2abb436c5
Priyanshu Singh <priyanshusing8000@gmail.com>
2024-09-09 20:56:21 +0530

    index.js

--- Video 60/index.js
+++ Video 60/index.js
@@ -5,21 +5,25 @@ console.log(a[1]);
 console.log(a[2]);
 console.log(a[3]);
 console.log(a[4]);
-// console.log(a[5]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+console.log(a[8]);
+// console.log(a[9]);
 
-console.log(a.length)
+console.log(a.length);
 
-let real_name = "Priyanshu"
-let friend = "Rohan"
-console.log("His name is " + real_name + " and his friends name is " + friend)
-console.log(`His name is ${real_name} and his friends name is ${friend}`)
+let real_name = "Priyanshu";
+let friend = "Rohan";
+console.log("His name is " + real_name + " and his friends name is " + friend);
+console.log(`His name is ${real_name} and his friends name is ${friend}`);
 
 let b = "ShivamSh"
-console.log(b.toUpperCase())
-console.log(b.toLowerCase()) 
-console.log(b.length) 
-console.log(b.slice(1, 5)) 
-console.log(b.slice(1)) 
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1, 5));
+console.log(b.slice(1));
 
 console.log(b.replace("Sh", "77"))
 console.log(b.concat(a, "Aishwariya", "Rahul", "Priya"))
